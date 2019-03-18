00059505944082f43871c281bd131874ca970c02
Hasan Burhan Beytur <hasanburhan@gmail.com>
2019-03-18 09:55:37 +0300

    __update__

--- Network.py
+++ Network.py
@@ -1,10 +1,50 @@
 import numpy as np
+from functools import wraps
+import inspect
+
+def initializer(func):
+    """
+    Automatically assigns the parameters.
+
+    >>> class process:
+    ...     @initializer
+    ...     def __init__(self, cmd, reachable=False, user='root'):
+    ...         pass
+    >>> p = process('halt', True)
+    >>> p.cmd, p.reachable, p.user
+    ('halt', True, 'root')
+    """
+    names, varargs, keywords, defaults = inspect.getargspec(func)
+
+    @wraps(func)
+    def wrapper(self, *args, **kargs):
+        for name, arg in list(zip(names[1:], args)) + list(kargs.items()):
+            setattr(self, name, arg)
+
+        for name, default in zip(reversed(names), reversed(defaults)):
+            if not hasattr(self, name):
+                setattr(self, name, default)
+
+        func(self, *args, **kargs)
+
+    return wrapper
+
 
 class Network(object):
-    def __init__(self):
+    @initializer
+    def __init__(self,
+    num_source = 5,
+    num_server = 1,
+    num_packet = 1000,
+    arrival = ("poisson",[1]*5),
+    service = ("exponential",[0.1]*5),
+    queue = "FCFS",
+    preemption = False,
+    scheduler = "MAF"):
 
-        self.num_server = 1
-        self.num_source = 5
+        self.generator = self.Packet_Generator(num_packet,num_source,arrival)
+        self.queue = self.Queue(queue)
+        self.scheduler = self.
 
     def generateSampleTimes(self,num_packet = 1000, type = "poisson", rate="default"):
         if rate == "default":
@@ -19,6 +59,28 @@ class Network(object):
 
         self.arr_TS = np.cumsum(self.arr_TS,0)
 
+    class Queue(object):
+        def __init__(self):
+            return
+        def in(self):
+            return
+        def out(self):
+            return
+
+    class Packet_Generator(object):
+        def __init__(self,num_packet,num_source,arrival,seed = 0):
+            if arrival[0] == "poisson":
+                self.arr = np.random.exponential(arrival[1],(num_packet,num_source))
+            elif arrival[0] == "deterministic":
+                self.arr = np.ones(num_packet,num_source)*arrival[1]
+
+            self.arr = np.cumsum(self.arr,0)
+
+
+            return
+    class Scheduler(object):
+        def __init__(self):
+            pass
 
 
 if __name__ == "__main__":
